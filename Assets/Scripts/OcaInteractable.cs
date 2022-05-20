49739370d52e97fdcd50bab3252af2431550e915
Ad <adlan.sadou@epitech.eu>
2022-05-20 12:25:15 +0200

    added OcaInteractable
    This class inherits MonoBehaviour and should be used in place of it for objects that need to have controller HUD

---
+++ Assets/Scripts/OcaInteractable.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public enum ElementType
+{
+    Label,
+    Toggle,
+    Slider,
+}
+
+[Serializable]
+public class GuiElementDescriptor
+{
+    public ElementType elementType;
+    public string fieldName;
+    public string displayName;
+    // <summary>
+    // Provide fieldName as is, otherwise the reflection system will not be able to find it
+    // <summary>
+    public GuiElementDescriptor(ElementType type, string exactFieldName, string displayName = null)
+    {
+        this.elementType = type;
+        this.fieldName = exactFieldName;
+        this.displayName = displayName;
+    }
+}
+
+public class OcaInteractable : MonoBehaviour
+{
+    // maps fields to ui elements
+    [HideInInspector]
+    public List<GuiElementDescriptor> guiElementsDescriptor;
+    private OcaControllerHUD _HUD;
+
+    private bool IsValidFieldName(string fieldName)
+    {
+        if (string.IsNullOrEmpty(fieldName) || string.IsNullOrWhiteSpace(fieldName))
+        {
+            Debug.LogError("FieldName is empty or invalid");
+            return false;
+        }
+
+        if (this.GetType().GetField(fieldName) == null)
+        {
+            Debug.LogError("Cannot find field named '" + fieldName + "'");
+            return false;
+        }
+
+        return true;
+    }
+
+    public object GetValueByFieldName(string fieldName)
+    {
+
+        if (!IsValidFieldName(fieldName))
+            return null;
+
+        return this.GetType().GetField(fieldName).GetValue(this);
+    }
+
+    public void InjectHUDReference(OcaControllerHUD HUD) {
+        this._HUD = HUD;
+    }
+}
